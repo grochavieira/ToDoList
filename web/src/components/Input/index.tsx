3661b99489b780652b2b3876d00f91a96fd969bf
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2020-10-05 19:04:33 -0300

    can now get input data

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -6,12 +6,27 @@ interface InputProps {
   name: string;
   label: string;
   placeholder: string;
+  value: string;
+  setValue: (value: string) => void;
 }
 
-const Input: React.FC<InputProps> = ({ name, label, placeholder }) => {
+const Input: React.FC<InputProps> = ({
+  name,
+  label,
+  placeholder,
+  value,
+  setValue,
+}) => {
   return (
     <div className="input-block">
-      <input name={name} type="text" placeholder={placeholder} required />
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        name={name}
+        type="text"
+        placeholder={placeholder}
+        required
+      />
       <label htmlFor={name}>{label}</label>
     </div>
   );
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 import "./styles.css";
 
 const Home = () => {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+  }
+
   return (
     <div className="home-container">
       <main className="todo-container">
         <h1>To Do List</h1>
-        <form action="#">
-          <Input name="todo" label="todo" placeholder="todo" />
+        <form onSubmit={handleSubmit}>
+          <Input
+            value={todo}
+            setValue={setTodo}
+            name="todo"
+            label="todo"
+            placeholder="todo"
+          />
           <Button name="Criar ToDo" />
         </form>
+
+        <div className="todo-list-cotainer"></div>
       </main>
     </div>
   );
